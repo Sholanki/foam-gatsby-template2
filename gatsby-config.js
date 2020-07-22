a5cf31d76ff5280ee78257d02400bc762994fa3a
Sholanki <63105386+Sholanki@users.noreply.github.com>
2020-07-22 19:16:22 +0530

    Add files via upload

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  pathPrefix: ``,
+  siteMetadata: {
+    title: `Foam`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-theme-garden`,
+      options: {
+        rootNote: "/readme",
+        contentPath: `${__dirname}/..`,
+        ignore: [
+          "**/_layouts/**",
+          "**/.git/**",
+          "**/.github/**",
+          "**/.vscode/**",
+		  "/netlify.toml"
+        ],
+      },
+    },
+  ],
+};
